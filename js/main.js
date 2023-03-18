9eedc0645cdf3a8bde8b4b2fa98ca9cf3b64e116
javierbull <javierbullreyes@gmail.com>
2023-03-18 15:13:53 -0500

    base

---
+++ js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(document).ready(function () {
+
+  $("#show-resumen").click(function () {
+    $("#content-sorteo").hide();
+    $("#content-resumen").show();
+  });
+});
+
+// End Drag & Drop
