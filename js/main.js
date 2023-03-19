7985a39df5c8d496c94963e8b981481d3dde79a6
javierbull <javierbullreyes@gmail.com>
2023-03-18 23:47:53 -0500

    calculadora gastos arbitrales

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@
 
 $(document).ready(function () {
 
+   // TOOLTIP
+   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+     return new bootstrap.Tooltip(tooltipTriggerEl)
+   })
+   // END TOOLTIP
+
   $("#show-resumen").click(function () {
     $("#content-sorteo").hide();
     $("#content-resumen").show();
