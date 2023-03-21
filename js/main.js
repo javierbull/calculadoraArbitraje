24790db4f3c96a02e0cb88bc227a188a75bd0bee
javierbull <javierbullreyes@gmail.com>
2023-03-20 23:48:32 -0500

    resultado arbitraje de emergencia

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@
 
 $(document).ready(function () {
 
-   // TOOLTIP
-   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-     return new bootstrap.Tooltip(tooltipTriggerEl)
-   })
-   // END TOOLTIP
+  // TOOLTIP
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+  // END TOOLTIP
+
 
   $("#pills-gastos-arbitrales-tab").click(function () {
     $("#header-gastos-arbitrales").show();
@@ -18,6 +19,18 @@ $(document).ready(function () {
     $("#header-gastos-arbitrales").hide();
     $("#header-arbitraje-emergencia").show();
   });
+
+  $("#btn-calcular-gastos-arbitrales").click(function () {
+    $("#section-resultados-gastos-arbitrales").show();
+    $("#section-resultados-arbitraje-emergencia").hide();
+  });
+
+  $("#btn-calcular-arbitraje-emergencia").click(function () {
+    $("#section-resultados-gastos-arbitrales").hide();
+    $("#section-resultados-arbitraje-emergencia").show();
+  });
+
+
 });
 
-// End Drag & Drop
+
